d15783b99f64fd8cd4d251266ff480423e8f1e66
John Allen <john_allen3@student.uml.edu>
2017-01-24 15:06:14 -0500

    add schedules to the app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124183338) do
+ActiveRecord::Schema.define(version: 20170124195124) do
 
   create_table "AtBatPlays", primary_key: "MasterID", id: :string, limit: 50, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.bigint   "Id"
@@ -6326,6 +6326,16 @@ ActiveRecord::Schema.define(version: 20170124183338) do
     t.integer  "format",                                                       null: false
     t.integer  "winner_id"
     t.integer  "winner_uid"
+  end
+
+  create_table "schedules", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "game_time"
+    t.integer  "sid"
+    t.integer  "away_team_id"
+    t.integer  "home_team_id"
+    t.integer  "remote_tournament_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
 end
