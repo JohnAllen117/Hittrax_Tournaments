15691bdf3b5293a8cf7cd1bcc25f61de5709d8fd
John Allen <john_allen3@student.uml.edu>
2017-01-30 12:21:21 -0500

    migration fixes

--- db/migrate/20170124183338_create_remote_tournaments.rb
+++ db/migrate/20170124183338_create_remote_tournaments.rb
@@ -5,12 +5,13 @@ class CreateRemoteTournaments < ActiveRecord::Migration[5.0]
       t.integer  :company_id, null: false
       t.integer  :playoff_type,                                                 null: false
       t.string   :name, limit: 50, default: "", null: false
-      t.datetime :ts, default: -> { "CURRENT_TIMESTAMP" }, null: false
       t.date     :start_date, null: false
       t.integer  :finished, default: 0, null: false
       t.integer  :format, null: false
       t.integer  :winner_id
       t.integer  :winner_uid
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20170124200759_create_remote_tournaments_teams.rb
+++ db/migrate/20170124200759_create_remote_tournaments_teams.rb
@@ -3,8 +3,6 @@ class CreateRemoteTournamentsTeams < ActiveRecord::Migration[5.0]
     create_table :remote_tournaments_teams do |t|
       t.integer :remote_tournament_id, presence: true
       t.integer :team_id, presence: true
-
-      t.timestamps
     end
   end
 end
