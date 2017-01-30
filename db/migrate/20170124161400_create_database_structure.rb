3ce86696845f1db4b246f19fab0b818a841f3ee1
John Allen <john_allen3@student.uml.edu>
2017-01-30 13:49:08 -0500

    logic fixing

--- db/migrate/20170124161400_create_database_structure.rb
+++ db/migrate/20170124161400_create_database_structure.rb
@@ -10,7 +10,7 @@ class CreateDatabaseStructure < ActiveRecord::Migration[5.0]
   # you'll amass, the slower it'll run and the greater likelihood for issues).
   #
   # It's strongly recommended that you check this file into your version control system.
-  unless Rails.env == 'production'
+  if Rails.env == 'development' || Rails.env =='test'
     ActiveRecord::Schema.define(version: 20170113034144) do
 
       create_table "AtBatPlays", primary_key: "MasterID", id: :string, limit: 50, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
