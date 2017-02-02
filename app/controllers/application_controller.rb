3c7fdf0dc64c0bcd0074edcff6de4a19e1507001
John Allen <john_allen3@student.uml.edu>
2017-02-02 15:23:37 -0500

    added access password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  http_basic_authenticate_with name: "hittrax", password: "vKSa4k!B"
   protect_from_forgery with: :exception
   helper_method :current_user
 
