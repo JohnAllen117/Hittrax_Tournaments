e457b7b83bcc571192278f4cfa5ac9ebf428f582
John Allen <john_allen3@student.uml.edu>
2017-03-02 15:02:15 -0500

    pre master pull commit

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -4,10 +4,12 @@ class FacilitiesController < ApplicationController
   end
 
   def edit
-
+    @facility = current_user.facility
   end
 
   def update
+    binding.pry
+    @facility = Facility.find_by(params[:id])
 
   end
 end
